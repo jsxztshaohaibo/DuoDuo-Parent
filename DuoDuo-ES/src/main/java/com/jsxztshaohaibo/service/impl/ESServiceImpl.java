fe5b466fc3298581a07b1ea164ae3b13f3cf8667
haibo.hao <haibo.hao@becom.com.cn>
2021-02-18 15:33:38 +0800

    ES修改

--- DuoDuo-ES/src/main/java/com/jsxztshaohaibo/service/impl/ESServiceImpl.java
+++ DuoDuo-ES/src/main/java/com/jsxztshaohaibo/service/impl/ESServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jsxztshaohaibo.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +26,9 @@ public class ESServiceImpl implements IESService {
 	 */
 	@Override
 	public String delete(String id) {
+		if(StringUtils.isBlank(id)){
+			esMapper.deleteAll();
+		}
 		esMapper.deleteById(id);
 		return "ok";
 	}
